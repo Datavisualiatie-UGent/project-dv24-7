26bda68967e3594eb7875b2cf4f253e9232dee31
viktor40 <vannieuwenhuize.viktor@gmail.com>
2024-04-22 16:24:48 +0200

    viktor changes

--- site/docs/components/renderers.js
+++ site/docs/components/renderers.js
@@ -192,3 +192,198 @@ export const complexity = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {
     
     return svg.node();
 };
+
+const cards_per_set = (cards_list) => {
+    let n_cards = 0;
+
+        cards_list.forEach(card => {
+        n_cards++;
+    });
+    return n_cards;
+}
+
+
+const get_color_by_code = (code) => {
+    switch (code) {
+        case 'W':
+            return 'white';
+
+        case 'U':
+            return 'blue';
+        
+        case 'B':
+            return 'black';
+
+        case 'R':
+            return 'red';
+            
+        case 'G':
+            return 'green';
+    }
+}
+
+const card_color_list = {
+    colorless: 0,
+    green: 0,
+    red: 0,
+    black: 0,
+    blue: 0,
+    white: 0,
+    mixed: 0,
+}
+
+
+const color_set_year = (year_sets, cards) => {
+    year_sets.forEach(set => {
+        const cards_list = cards[set.code].flat().filter(card => card.colors != null).map(card => card.colors);
+        cards_list.forEach(colors => {
+            if (colors.length == 0) {
+                card_color_list.colorless++;
+            } else if (colors.length > 1) {
+                card_color_list.mixed++;
+            } else {
+                card_color_list[get_color_by_code(colors[0])]++;
+            }
+        });
+    })
+    return card_color_list;
+}
+
+
+export const color_per_set_per_year = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
+    const actual = filter_years(sets, from, to);
+    const year_to_count = [];
+    for(let i = Math.max(from, sets[0].release.getFullYear()); i <= Math.min(to, sets.at(-1).release.getFullYear()); i++) {
+        const year_sets = actual.filter(set => set.release.getFullYear() === i);
+        const color_counts = color_set_year(year_sets, cards);
+
+        Object.keys(color_counts).forEach(key => {
+            year_to_count.push({
+                year: i,
+                color: key,
+                color_count: color_counts[key]
+            });
+        })
+    }
+
+    const data = year_to_count;
+    const x = d3.scaleBand().range([marginLeft, w - marginRight]).domain(year_to_count.map(year => year.year)).padding(0.1);
+    const y = mk_lin_y(h, [0, Math.max(...year_to_count.map(year => year.color_count.total_cards))]);
+    const svg = d3.create("svg").attr("width", w).attr("height", h);
+
+    // var stacked_data = d3.stack()
+    //     .keys(Object.keys(card_color_list))
+    //     .value(([, group], key) => group.get(key).color_count)
+    //     (d3.index(data, d => d.date, d => d.color));
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${h - marginBottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('g')
+        .attr('transform', `translate(${marginLeft}, 0)`)
+        .call(d3.axisLeft(y));
+
+    return year_to_count;
+}
+
+export const cards_per_set_per_year = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
+    const actual = [];
+    filter_years(sets, from, to).sort((a, b) => a.release - b.release).forEach(set => {
+        const cards_list = cards[set.code].flat();
+        const n_cards = cards_per_set(cards_list);
+
+       actual.push({
+          set: set.code,
+          release: set.release,
+          n: n_cards
+       });
+    });
+
+    const x = mk_utc_x(w, d3.extent(actual, set => set.release));
+    const y = mk_lin_y(h, [Math.min(...actual.map(s => s.n)), Math.max(...actual.map(s => s.n))]);
+    const svg = d3.create("svg").attr("width", w).attr("height", h);
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${h - marginBottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('g')
+        .attr('transform', `translate(${marginLeft}, 0)`)
+        .call(d3.axisLeft(y));
+
+    svg.append('path')
+        .datum(actual)
+        .attr('fill', 'none')
+        .attr('stroke', 'navy')
+        .attr('stroke-width', 2)
+        .attr('d', d3.line()
+            .x(d => x(d.release))
+            .y(d => y(d.n))
+        );
+
+    return svg.node();
+}
+
+
+
+
+export const cards_per_year = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
+    const actual = filter_years(sets, from, to);
+    const year_to_count = [];
+    for(let i = Math.max(from, sets[0].release.getFullYear()); i <= Math.min(to, sets.at(-1).release.getFullYear()); i++) {
+        const year_sets = actual.filter(set => set.release.getFullYear() === i);
+        let cards_in_set = 0;
+        year_sets.forEach(set => {
+            const cards_list = cards[set.code].flat();
+            cards_in_set += cards_per_set(cards_list);
+        })
+
+        year_to_count.push({
+            year: i, 
+            count: year_sets.length,
+            sets: year_sets,
+            cards_in_set: Math.round(cards_in_set)
+        });
+    }
+
+    const x = d3.scaleBand().range([marginLeft, w - marginRight]).domain(year_to_count.map(year => year.year)).padding(0.1);
+    const y = mk_lin_y(h, [0, Math.max(...year_to_count.map(year => year.cards_in_set))]);
+    const svg = d3.create("svg").attr("width", w).attr("height", h);
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${h - marginBottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('g')
+        .attr('transform', `translate(${marginLeft}, 0)`)
+        .call(d3.axisLeft(y));
+
+    svg.selectAll('bar')
+        .data(year_to_count)
+        .enter()
+        .append('rect')
+        .attr('x', d => x(d.year))
+        .attr('y', d => y(d.cards_in_set))
+        .attr('width', x.bandwidth())
+        .attr('height', d => h - marginBottom - y(d.cards_in_set))
+        .attr('fill', 'steelblue');
+
+    const reg = regression.polynomial(year_to_count.map(year => [year.year - year_to_count[0].year, year.cards_in_set]), { order: 2 });
+    const reg_line = [];
+    for(let i = year_to_count[0].year; i <= year_to_count.at(-1).year; i++) {
+        reg_line.push({ year: i, cards_in_set: reg.predict(i - year_to_count[0].year)[1] });
+    }
+
+    svg.append('path')
+        .datum(reg_line)
+        .attr('fill', 'none')
+        .attr('stroke', 'maroon')
+        .attr('stroke-width', 2)
+        .attr('d', d3.line()
+            .x(d => x(d.year))
+            .y(d => y(d.cards_in_set))
+        );
+
+    return svg.node();
+}
---
+++ site/docs/viktor-test.md
@@ -0,0 +1,54 @@
+---
+title: Small Test Viktor
+---
+
+```js
+const exclude = [
+  'alchemy', 'archenemy', 'duel_deck', 'from_the_vault', 'funny', 
+  'masterpiece', 'memorabilia', 'minigame', 'premium_deck', 'promo',
+  'spellbook', 'token', 'treasure_chest', 'vanguard'  
+];
+
+const preproc_sets = (sets) => {
+    return sets.map(set => {
+        return {
+            ...set,
+            release: new Date(set.released_at)
+        };
+    }).sort((a, b) => a.release - b.release);
+};
+const remove_excluded_sets = (sets) => {
+    return sets.filter(set => set.parent_set_code === undefined).filter(set => !exclude.includes(set.set_type));
+};
+
+const sets = preproc_sets(remove_excluded_sets((await FileAttachment("./data/sets.json").json()).data));
+
+const load_cards = async () => {
+    const cards = {};
+    const card_zip = await FileAttachment("./data/cards.zip").zip();
+    await Promise.all(sets.map(async set => {
+        cards[set.code] = await card_zip.file(`out/${set.code}.json`).json();
+    }));
+    console.log(cards);
+    return cards;
+};
+const cards_by_sets = await load_cards();
+```
+
+## Evolution of color distribution
+```js
+import {color_per_set_per_year} from './components/renderers.js';
+display(color_per_set_per_year(sets, cards_by_sets));
+```
+
+## Evolution of set size
+```js
+import {cards_per_set_per_year} from './components/renderers.js';
+display(cards_per_set_per_year(sets, cards_by_sets));
+```
+
+## Number of cards released per year
+```js
+import {cards_per_year} from './components/renderers.js';
+display(cards_per_year(sets, cards_by_sets));
+```
