5a5690053d084da2034b43349d5845d2c8758169
jay-tux <sys.jonas@yahoo.com>
2024-04-22 13:53:00 +0200

    More graphs! <3

--- site/docs/components/renderers.js
+++ site/docs/components/renderers.js
@@ -17,6 +17,8 @@ const diff_months = (d1, d2) => {
 
 const filter_years = (sets, from, to) => sets.filter(set => set.release.getFullYear() >= from && set.release.getFullYear() <= to);
 
+const days_since = (d1, d2) => (d2 - d1) / (1000 * 60 * 60 * 24);
+
 export const set_release_time = (sets, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
     const actual = filter_years(sets, from, to)
         .map((v, i) => ({ ...v, idx: i, diff: i === 0 ? 0 : diff_months(sets[i - 1].release, v.release) }));
@@ -144,23 +146,63 @@ export const sets_per_year = (sets, {w, h} = {w:1200, h:600}, {from, to} = {from
 export const complexity = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
     const actual = [];
     filter_years(sets, from, to).sort((a, b) => a.release - b.release).forEach(set => {
-       const compl = cards[set.code].flat().filter(card => card.oracle_text != null).map(card => card.oracle_text.split(' ').length);
+       const compl = cards[set.code]
+           .flat()
+           .map(card => {
+               if(card.card_faces != null) {
+                   let text = '';
+                   card.card_faces.forEach(face => text += face.oracle_text + ' ');
+                   return { ...card, oracle_text: text };
+               }
+               else if(card.oracle_text == null) {
+                   return { ...card, oracle_text: '' };
+               }
+               else {
+                   return card;
+               }
+           })
+           .map(card => card.oracle_text.split(' ').length);
        const min = Math.min(...compl);
        const max = Math.max(...compl);
        const avg = d3.mean(compl);
 
        actual.push({
-          set: set.code,
-          release: set.release,
-          min: min,
-          max: max,
-          avg: avg
+           name: set.name,
+           set: set.code,
+           release: set.release,
+           avg: avg
        });
     });
 
     const x = mk_utc_x(w, d3.extent(actual, set => set.release));
-    const y = mk_lin_y(h, [Math.min(...actual.map(s => s.min)), Math.max(...actual.map(s => s.max))]);
+    const y = mk_lin_y(h, [0, Math.max(...actual.map(set => set.avg))]);
     const svg = d3.create("svg").attr("width", w).attr("height", h);
+
+    const tooltip = d3.select("body")
+        .append("div")
+        .style("visibility", "hidden")
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("width", "250px")
+        .style("height", "auto")
+        .style("position", "absolute");
+
+    const mouse_over = (d) => {
+        tooltip.style("visibility", "visible");
+    }
+    const mouse_move = (event, d) => {
+        tooltip.html(`Set ${d.name} (Code ${d.set})<br>Average ${d.avg.toFixed(2)} words/card.`)
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY) + "px");
+    }
+
+    const mouse_leave = (d) => {
+        tooltip.style("visibility", "hidden");
+    }
 
     svg.append('g')
         .attr('transform', `translate(0, ${h - marginBottom})`)
@@ -173,22 +215,196 @@ export const complexity = (sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {
     svg.append('path')
         .datum(actual)
         .attr('fill', 'none')
-        .attr('stroke', 'navy')
-        .attr('stroke-width', 2)
-        .attr('d', d3.line()
-            .x(d => x(d.release))
-            .y(d => y(d.max))
-        );
-
-    svg.append('path')
-        .datum(actual)
-        .attr('fill', 'none')
         .attr('stroke', 'maroon')
         .attr('stroke-width', 2)
         .attr('d', d3.line()
             .x(d => x(d.release))
             .y(d => y(d.avg))
         );
+
+    svg.append('g')
+        .selectAll('circle')
+        .data(actual)
+        .join('circle')
+        .attr('stroke', 'steelblue')
+        .attr('fill', 'currentColor')
+        .attr('opacity', 0.5)
+        .attr('cx', d => x(d.release))
+        .attr('cy', d => y(d.avg))
+        .attr('r', 5)
+        .on("mouseover", mouse_over)
+        .on("mousemove", mouse_move)
+        .on("mouseleave", mouse_leave);
+
+
+    const reg = regression.polynomial(actual.map(set => [days_since(actual[0].release, set.release), set.avg]), { order: 2, precision: 7 });
+    const reg_line = actual.map(set => ({ release: set.release, avg: reg.predict(days_since(actual[0].release, set.release))[1] }))
+
+    svg.append('path')
+        .datum(reg_line)
+        .attr('fill', 'none')
+        .attr('stroke', 'navy')
+        .attr('stroke-width', 2)
+        .attr('d', d3.line()
+            .x(d => x(d.release))
+            .y(d => y(d.avg))
+        );
     
+    return svg.node();
+};
+
+export const complexity_by_kind = (kinds, unfiltered_sets, cards, {w, h} = {w:1200, h:600}, {from, to} = {from:1990, to:2030}) => {
+    const mapped = {};
+    kinds.forEach(k => { mapped[k] = [] });
+
+    filter_years(unfiltered_sets, from, to).sort((a, b) => a.release - b.release).forEach(set => {
+        const compl = cards[set.code]
+            .flat()
+            .filter(card => card != null)
+            .map(card => {
+                if(card.card_faces != null) {
+                    let text = '';
+                    card.card_faces.forEach(face => text += face.oracle_text + ' ');
+                    return { ...card, oracle_text: text };
+                }
+                else if(card.oracle_text == null) {
+                    return { ...card, oracle_text: '' };
+                }
+                else {
+                    return card;
+                }
+            })
+            .map(card => card.oracle_text.split(' ').length);
+        const min = Math.min(...compl);
+        const max = Math.max(...compl);
+        const avg = d3.mean(compl);
+
+        mapped[set.set_type].push({
+            name: set.name,
+            set: set.code,
+            release: set.release,
+            avg: avg,
+            type: set.set_type
+        });
+    });
+
+    const release_dates = Object.values(mapped).flat().map(x => x.release)
+    const complexities = Object.values(mapped).flat().map(x => x.avg).filter(x => x != undefined);
+
+    const x = mk_utc_x(w, d3.extent(release_dates));
+    const y = mk_lin_y(h, [0, Math.max(...complexities)]);
+    const svg = d3.create("svg").attr("width", w).attr("height", h);
+
+    const tooltip = d3.select("body")
+        .append("div")
+        .style("visibility", "hidden")
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("width", "250px")
+        .style("height", "auto")
+        .style("position", "absolute");
+
+    const mouse_over = (d) => {
+        tooltip.style("visibility", "visible");
+    }
+    const mouse_move = (event, d) => {
+        tooltip.html(`Set ${d.name} (Code ${d.set})<br>Average ${d.avg.toFixed(2)} words/card.<br>Set type: ${d.type}`)
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY) + "px");
+    }
+
+    const ids = kinds.map(kind => `line-${kind}`);
+    const circ_ids = kinds.map(kind => `circle-${kind}`);
+    const color_for = (kind) => d3.interpolateTurbo(kinds.indexOf(kind) / kinds.length);
+
+    let last_enter = '';
+
+    const mouse_move_line = (event, d) => {
+        tooltip.html(`Set kind: ${d[0].type}`)
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY) + "px");
+        ids.forEach(id => d3.select(`#${id}`)
+            .transition()
+            .style('opacity', id === `line-${d[0].type}` ? 1.0 : 0.2));
+
+        circ_ids.forEach(id => d3.selectAll(`.${id}`).transition().style('opacity', id === `circle-${d[0].type}` ? 1.0 : 0.0));
+
+        d3.select(`#line-${d[0].type}`).transition().style('stroke', 'black');
+        d3.select(`#reg-line-${d[0].type}`).transition().style('visibility', 'visible');
+        last_enter = d[0].type;
+    }
+
+    const mouse_leave = (d) => {
+        tooltip.style("visibility", "hidden");
+    }
+
+    const mouse_leave_line = (d) => {
+        tooltip.style("visibility", "hidden");
+        ids.forEach(id => d3.select(`#${id}`).transition().style('opacity', 1.0));
+        d3.select(`#line-${last_enter}`).transition().style('stroke', color_for(last_enter));
+        circ_ids.forEach(id => d3.selectAll(`.${id}`).transition().style('opacity', 0.25));
+        d3.select(`#reg-line-${last_enter}`).transition().style('visibility', 'hidden');
+    }
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${h - marginBottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('g')
+        .attr('transform', `translate(${marginLeft}, 0)`)
+        .call(d3.axisLeft(y));
+
+    Object.keys(mapped).forEach(kind => {
+        const datum = mapped[kind];
+
+        svg.append('path')
+            .datum(datum)
+            .attr('fill', 'none')
+            .attr('stroke', color_for(kind))
+            .attr('stroke-width', 3)
+            .attr('d', d3.line()
+                .x(d => x(d.release))
+                .y(d => y(d.avg))
+            )
+            .attr('id', `line-${kind}`)
+            .on('mouseover', mouse_over)
+            .on('mousemove', mouse_move_line)
+            .on('mouseleave', mouse_leave_line);
+
+        svg.append('g')
+            .selectAll('circle')
+            .data(datum)
+            .join('circle')
+            .attr('stroke', 'steelblue')
+            .attr('fill', 'currentColor')
+            .attr('class', `circle-${kind}`)
+            .attr('opacity', 0.25)
+            .attr('cx', d => x(d.release))
+            .attr('cy', d => y(d.avg))
+            .attr('r', 3.5)
+            .on("mouseover", mouse_over)
+            .on("mousemove", mouse_move)
+            .on("mouseleave", mouse_leave);
+
+        const reg = regression.polynomial(datum.map(set => [days_since(datum[0].release, set.release), set.avg]), { order: 2, precision: 7 });
+        const reg_line = datum.map(set => ({ release: set.release, avg: reg.predict(days_since(datum[0].release, set.release))[1] }))
+
+        svg.append('path')
+            .datum(reg_line)
+            .attr('fill', 'none')
+            .attr('stroke', 'navy')
+            .attr('stroke-width', 2)
+            .attr('visibility', 'hidden')
+            .attr('id', `reg-line-${kind}`)
+            .attr('d', d3.line()
+                .x(d => x(d.release))
+                .y(d => y(d.avg))
+            );
+    });
+
     return svg.node();
 };
--- site/docs/jay-test.md
+++ site/docs/jay-test.md
@@ -3,6 +3,8 @@ title: Small Test Jonas
 ---
 
 ```js
+const exclude_always = [ 'minigame', 'token', 'promo', 'memorabilia', 'treasure_chest', 'vanguard' ];
+
 const exclude = [
   'alchemy', 'archenemy', 'duel_deck', 'from_the_vault', 'funny', 
   'masterpiece', 'memorabilia', 'minigame', 'premium_deck', 'promo',
@@ -21,15 +23,17 @@ const remove_excluded_sets = (sets) => {
     return sets.filter(set => set.parent_set_code === undefined).filter(set => !exclude.includes(set.set_type));
 };
 
-const sets = preproc_sets(remove_excluded_sets((await FileAttachment("./data/sets.json").json()).data));
+const sets_raw = preproc_sets((await FileAttachment("./data/sets.json").json()).data)
+    .filter(x => !exclude_always.includes(x.set_type));
+const sets = remove_excluded_sets(sets_raw);
+const set_kinds = [...new Set(sets_raw.map(set => set.set_type))]; 
 
 const load_cards = async () => {
     const cards = {};
     const card_zip = await FileAttachment("./data/cards.zip").zip();
-    await Promise.all(sets.map(async set => {
+    await Promise.all(sets_raw.map(async set => {
         cards[set.code] = await card_zip.file(`out/${set.code}.json`).json();
     }));
-    console.log(cards);
     return cards;
 };
 const cards_by_sets = await load_cards();
@@ -51,4 +55,10 @@ display(sets_per_year(sets));
 ```js
 import {complexity} from './components/renderers.js';
 display(complexity(sets, cards_by_sets));
+```
+
+## Evolution of set complexity by set kind
+```js
+import {complexity_by_kind} from './components/renderers.js';
+display(complexity_by_kind(set_kinds, sets_raw, cards_by_sets));
 ```
